400fa4fa175c0733b3cbd85f21cd5d5ee911de17
Mohan <mkarthi182003@gmail.com>
2025-12-14 23:43:08 -0500

    add logout functionality

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,13 +1,24 @@
 import "./layout.css";
+import { useNavigate } from "react-router-dom";
+import { logout } from "../../api/authService";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
   return (
     <header className="navbar">
       <h3 className="page-title">Manager Dashboard</h3>
 
       <div className="profile">
         <span className="manager-name">Manager</span>
-        <button className="logout-btn">Logout</button>
+        <button className="logout-btn" onClick={handleLogout}>
+          Logout
+        </button>
       </div>
     </header>
   );
